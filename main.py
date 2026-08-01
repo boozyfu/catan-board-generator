4f442ffdd26b28451f7a9e8b8910dd4b76cff95f
boozyfu <boozyfu@protonmail.com>
2026-08-01 16:32:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 import glob
 from PIL import Image
-from Backend.board_generator import fp_resources, tp_resources, display_board 
+from Backend.board_generator import fp_resources, tp_resources, create_catan_board 
 
 tile_size = 120
 fp, tp, exp, help = st.tabs(["4-Person", "2-Person", "Expansion", "Help"])
@@ -14,4 +14,4 @@ with fp:
     with st.bottom:
       generate_btn = st.button("Generate Board")
     if generate_btn:
-      display_board(fp_resources)
+      create_catan_board(fp_resources)
