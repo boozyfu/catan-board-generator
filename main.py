51aec710c74c6ac28f380b99b55f3c1d71d1be6c
boozyfu <boozyfu@protonmail.com>
2026-08-01 08:58:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import streamlit as st
 import random
+import glob
+from PIL import Image
 
 tp, fp, exp, help = st.tabs(["2-Person", "4-Person", "Expansion", "Help"])
+
+if "image_files" not in st.session_state:
+  img_files = glob.glob("./Images/*.png")
+  st.session_state.image_files = img_files
+
+if "img_stem" not in st.session_state:
+  st.session_state.img_stem = [f.split("/")[-1] for f in st.session_state.image_files]
+
+@st.cache_data
+def load_board_images():
+  img_dict = {}
+  for file in st.session_state["image_files"]:
+    file_base = file.split("/")[-1]
+    with open(file, "r") as img_in:
+      img_load = Image.open(img_in)
+    img_dict[file_base] = img_load
+
+  return img_dict
+
+board_images = load_board_images()
 
 with tp:
   game_container = st.container()
@@ -9,6 +31,7 @@ with tp:
     st.header("Catan Board Generator - 2 Player")
     generate_btn = st.button("Generate Board")
     if generate_btn:
-      options = [1,2,3,4,5]
+      options = st.session_syate.img_stem
       choice = random.choice(options)
-      st.write(choice)
+      st.image(board_images[choice])
+      
