d43521f7257eb511d955861f62487c11975f5054
boozyfu <boozyfu@protonmail.com>
2026-08-01 15:25:49 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import random
 import glob
 from PIL import Image
-from Backend.board_generator import fp_resources, tp_resources, load_board_images, generate_board, display_grid 
+from Backend.board_generator import fp_resources, tp_resources, display_board 
 
 tile_size = 120
 fp, tp, exp, help = st.tabs(["4-Person", "2-Person", "Expansion", "Help"])
