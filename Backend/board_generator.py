f9333f4c0228b6c30476e7bb916ed2c3152d3e5c
boozyfu <boozyfu@protonmail.com>
2026-08-01 19:53:41 -0400

    Update board_generator.py

--- Backend/board_generator.py
+++ Backend/board_generator.py
@@ -64,15 +64,13 @@ def create_catan_board(resource_dict, game_type, tile_size=150):
     fp_numbers = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12]
 
     def generate_numbers(game_type):
-      num_list_lookup = {"fp": fp_numbers,
+        num_list_lookup = {"fp": fp_numbers,
                      "tp": tp_numbers}
-      st.write(game_type)
-      num_list = num_list_lookup[game_type]
+        num_list = num_list_lookup[game_type]
   
-      random.shuffle(num_list)
-      st.write(num_list)
+        random.shuffle(num_list)
 
-      return num_list
+        return num_list
   
     static_images = load_board_images()
 
