1c344ffda8ae9cba75e00d74fa9338c3cb545f3f
boozyfu <boozyfu@protonmail.com>
2026-08-02 01:31:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ if "image_files" not in st.session_state:
   st.session_state["image_files"] = glob.glob("./Images/*.png")
 
 
-
 with fp:
   st.markdown(
     """
@@ -27,3 +26,22 @@ with fp:
     if generate_btn:
       game_board = create_catan_board(fp_resources, "fp")
       render_image(game_board)
+
+with tp:
+  st.markdown(
+    """
+    <meta name="viewport" content="width=device-width, initial-scale=5.0, user-scalable=yes, maximum-scale=5.0">
+    """,
+    unsafe_allow_html=True
+)
+  st.header("Catan Board Generator - 2 Player")
+  game_container = st.container()
+  with game_container:
+    with st.bottom:
+      generate_btn = st.button("Generate Board")
+    if generate_btn:
+      game_board = create_catan_board(tp_resources, "tp")
+      render_image(game_board)
+
+
+  
